55e4387f940ded3a4a91503b2cf674ef9bbf22a6
Marisa Leung <mpleung93@yahoo.com>
2016-02-11 22:20:58 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,12 +12,16 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+	$('#exApi').click(exApi);
+
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+
 	// Prevent following the link
 	e.preventDefault();
 
@@ -26,7 +30,15 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+    $.get("/project/" + idNumber,callback);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function callback(result){
+  var id = "#project" + result.id;
+  var htmlStuff = "<h3>" +result.title+"</h3> <h4>" + result.date + "</h4>" + result.summary +  "<img class=\"detailsImage\" src = \"" + result.image + "\">";
+  $(id +  " .details").html(htmlStuff);
 }
 
 /*
@@ -35,4 +47,41 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", colorCallback);
+
+
+
+}
+
+/*
+ * Make an AJAX call to retrieve a color palette for the site
+ * and apply it
+ */
+function exApi(e) {
+	console.log("User clicked on exApi button");
+	var data = $.get("http://www.panoramio.com/map/get_panoramas.php?set=532693&from=0&to=20&minx=-180&miny=-90&maxx=180&maxy=90&size=medium&mapfilter=true", callbackFunction, 'jsonp');
+//	console.log(data);
+}
+
+function callbackFunction(result){
+	console.log(result);
+	console.log(result.count);
+	var res = result;
+	var str = "";
+	str = str + "From the example html   \n \"Count: " + res.count + "   \nhas_more: " + res.has_more + "  \nphotos: " +res.photos  + "\"";
+
+	$('h1').html(str);
+
+}
+
+
+function colorCallback(result){
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
